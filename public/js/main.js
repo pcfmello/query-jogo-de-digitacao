1b5153ff44044dfb4e083a2d33408f2581f38e11
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-08-17 21:36:10 -0300

    Função de tempo e desabilitar - Criada função que conta o tempo restante do jogador; - Criada função para bloquear o campo de texto e mostrar mensagem de fim de jogo;

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var frase = $('.frase').text();
 var numeroPalavras = frase.split(' ').length;
-var tamanhoFrase = $('.tamanho-frase');
+var tamanhoFrase = $('#tamanho-frase');
 tamanhoFrase.text(numeroPalavras);
 
 var campoDigitacao = $('.campo-digitacao');
@@ -10,4 +10,18 @@ campoDigitacao.on('input', function() {
 
   $('#contador-palavras').text(totalPalavras);
   $('#contador-caracteres').text(totalCaracteres);
+});
+
+campoDigitacao.one('focus', function() { // .one => Executa apenas uma vez o evento
+  var tempoDigitacao = $('#tempo-digitacao');
+  var tempoRestante = tempoDigitacao.text();
+  var cronometroId = setInterval(function() {
+        tempoRestante--;
+        tempoDigitacao.text(tempoRestante);
+        if(tempoRestante < 1) {
+            campoDigitacao.attr('disabled', true);
+            tempoDigitacao.parent().text('Tempo encerrado!');
+            clearInterval(cronometroId);
+        }
+    }, 1000);
 });
